267dd81176d196f3ee9bd222c926556145708ed8
erickmob <erickmob@gmail.com>
2019-10-10 14:43:22 -0300

    Hystrix and Sleuth added

--- gallery-service/src/main/java/com/erickmob/microservice/galleryservice/controller/HomeController.java
+++ gallery-service/src/main/java/com/erickmob/microservice/galleryservice/controller/HomeController.java
@@ -2,8 +2,10 @@ package com.erickmob.microservice.galleryservice.controller;
 
 
 import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.erickmob.microservice.galleryservice.entity.Gallery;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +23,8 @@ public class HomeController {
     @Autowired
     private Environment env;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
     @RequestMapping("/")
     public String home() {
         // This is useful for debugging
@@ -30,16 +34,23 @@ public class HomeController {
     }
 
     @RequestMapping("/{id}")
+    @HystrixCommand(fallbackMethod = "fallback")
     public Gallery getGallery(@PathVariable final int id) {
-        // create gallery object
+        LOGGER.info("Creating gallery object ... ");
         Gallery gallery = new Gallery();
         gallery.setId(id);
 
         // get list of available images
+        @SuppressWarnings("unchecked")    // we'll throw an exception from image service to simulate a failure
         List<Object> images = restTemplate.getForObject("http://image-service/images/", List.class);
         gallery.setImages(images);
-
+        LOGGER.info("Returning gallery ... ");
         return gallery;
+    }
+
+    // a fallback method to be called if failure happened
+    public Gallery fallback(int galleryId, Throwable hystrixCommand) {
+        return new Gallery(galleryId);
     }
 
     // -------- Admin Area --------
--- image-service/src/main/java/com/erickmob/microservice/imageservice/controller/HomeController.java
+++ image-service/src/main/java/com/erickmob/microservice/imageservice/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.erickmob.microservice.imageservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.erickmob.microservice.imageservice.entity.Image;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -12,15 +14,23 @@ import java.util.List;
 @RestController
 @RequestMapping("/")
 public class HomeController {
+
     @Autowired
     private Environment env;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
     @RequestMapping("/images")
-    public List<Image> getImages() {
+    public List<Image> getImages() throws Exception {
+        LOGGER.info("Creating images object ... ");
         List<Image> images = Arrays.asList(
                 new Image(1, "Treehouse of Horror V", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3842005760"),
                 new Image(2, "The Town", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3698134272"),
                 new Image(3, "The Last Traction Hero", "https://www.imdb.com/title/tt0096697/mediaviewer/rm1445594112"));
+
+        //Test for hystrix
+        //throw new Exception("Images can't be fetched");
+        LOGGER.info("Returning images ... ");
         return images;
     }
 }
